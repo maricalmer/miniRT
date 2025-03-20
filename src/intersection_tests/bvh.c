fcba764f747ee329fa3eea3b9666fd0159d33dbf
maricalmer <maricalmer@proton.me>
2025-03-20 02:00:29 +0100

    fix: bvh intersect getting wrong t value on fast_tr 0

--- src/intersection_tests/bvh.c
+++ src/intersection_tests/bvh.c
@@ -6,48 +6,55 @@
 /*   By: dlemaire <dlemaire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 20:22:11 by hruiz-fr          #+#    #+#             */
-/*   Updated: 2025/03/20 00:00:35 by dlemaire         ###   ########.fr       */
+/*   Updated: 2025/03/20 01:50:41 by dlemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static float	find_min_hit(float *res, t_bvh *bvh, int idx, t_shoot *shoot);
+static float	process_bvh_children(t_bvh *bvh, int idx, t_shoot *shoot,
+					float res[8]);
 
 float	visi_test_bvh_strict(t_bvh *bvh, int idx, t_shoot *shoot)
 {
-	__m256		v_res;
-	float		res[8];
-	float		t_min;
+	__m256	v_res;
+	float	res[8];
 
 	if (bvh->childs[idx] == -1)
 		return (visi_test_leafs(bvh->group[idx], shoot, bvh->group_size[idx]));
 	v_res = aabb_test_simd(bvh, bvh->childs[idx], shoot->dir, shoot->src);
 	_mm256_storeu_ps(res, v_res);
-	t_min = find_min_hit(res, bvh, idx, shoot);
-	if (t_min != FLT_MAX)
-		return (t_min);
-	return (0);
+	return (process_bvh_children(bvh, idx, shoot, res));
 }
 
-static float	find_min_hit(float *res, t_bvh *bvh, int idx, t_shoot *shoot)
+static float	process_bvh_children(t_bvh *bvh, int idx, t_shoot *shoot,
+	float res[8])
 {
-	int			i;
-	float		t;
 	float		t_min;
+	float		t;
+	int			i;
+	t_object	*obj;
 
-	i = -1;
 	t_min = FLT_MAX;
+	i = -1;
 	while (++i < 8)
 	{
 		if (res[i] != -1)
 		{
 			t = visi_test_bvh_strict(bvh, bvh->childs[idx] + i, shoot);
 			if (t > 0 && t < t_min)
+			{
 				t_min = t;
+				obj = shoot->obj;
+			}
 		}
 	}
-	return (t_min);
+	if (t_min != FLT_MAX)
+	{
+		shoot->obj = obj;
+		return (t_min);
+	}
+	return (0);
 }
 
 float	visi_test_bvh_fast(t_bvh *bvh, int idx, t_shoot *shoot)
@@ -79,8 +86,8 @@ float	shadow_test_bvh(t_shoot *shoot, t_bvh *bvh, int idx, float dist_light)
 	if (bvh->childs[idx] == -1)
 		return (shadow_test_leafs(shoot, bvh->group[idx], dist_light,
 				bvh->group_size[idx]));
-	v_res = aabb_test_simd(bvh, bvh->childs[idx], shoot->shadow_ray,
-			shoot->hit_pt);
+	v_res = aabb_test_simd(bvh, bvh->childs[idx], shoot->dir,
+			shoot->src);
 	_mm256_storeu_ps(res, v_res);
 	i = -1;
 	while (++i < 8)
--- src/intersection_tests/triangle.c
+++ src/intersection_tests/triangle.c
@@ -1,86 +1,86 @@
 #include "minirt.h"
 
-// static float	get_t(float s[3], float ray[3], float edges[6],
-// 					float det_and_bar[4]);
+static float	get_t(float s[3], float ray[3], float edges[6],
+					float det_and_bar[4]);
 
-// float	test_triangle(t_object *obj, float ray[3], float origin[3])
-// {
-// 	float	edges[6];
-// 	float	s[3];
-// 	float	h[3];
-// 	float	det_and_bar[4];
+float	test_triangle(t_object *obj, float ray[3], float origin[3])
+{
+	float	edges[6];
+	float	s[3];
+	float	h[3];
+	float	det_and_bar[4];
 
-// 	vec_substr(obj->geo.tri.v1, obj->geo.tri.v0, edges);
-// 	vec_substr(obj->geo.tri.v2, obj->geo.tri.v0, edges + 3);
-// 	cprod_13_13(ray, edges + 3, h);
-// 	det_and_bar[0] = dot_13_13(edges, h);
-// 	if (fabs(det_and_bar[0]) < EPSILON)
-// 		return (0.0f);
-// 	vec_substr(origin, obj->geo.tri.v0, s);
-// 	det_and_bar[1] = 1.0f / det_and_bar[0];
-// 	det_and_bar[2] = dot_13_13(s, h) * det_and_bar[1];
-// 	if (det_and_bar[2] < 0.0f || det_and_bar[2] > 1.0f)
-// 		return (0.0f);
-// 	return (get_t(s, ray, edges, det_and_bar));
-// }
+	vec_substr(obj->geo.tri.v1, obj->geo.tri.v0, edges);
+	vec_substr(obj->geo.tri.v2, obj->geo.tri.v0, edges + 3);
+	cprod_13_13(ray, edges + 3, h);
+	det_and_bar[0] = dot_13_13(edges, h);
+	if (fabs(det_and_bar[0]) < EPSILON)
+		return (0.0f);
+	vec_substr(origin, obj->geo.tri.v0, s);
+	det_and_bar[1] = 1.0f / det_and_bar[0];
+	det_and_bar[2] = dot_13_13(s, h) * det_and_bar[1];
+	if (det_and_bar[2] < 0.0f || det_and_bar[2] > 1.0f)
+		return (0.0f);
+	return (get_t(s, ray, edges, det_and_bar));
+}
 
-// static float	get_t(float s[3], float ray[3], float edges[6],
-// 	float det_and_bar[4])
-// {
-// 	float	q[3];
-// 	float	t;
+static float	get_t(float s[3], float ray[3], float edges[6],
+	float det_and_bar[4])
+{
+	float	q[3];
+	float	t;
 
-// 	cprod_13_13(s, edges, q);
-// 	det_and_bar[3] = dot_13_13(ray, q) * det_and_bar[1];
-// 	if (det_and_bar[3] < 0.0f || det_and_bar[2] + det_and_bar[3] > 1.0f)
-// 		return (0.0f);
-// 	t = dot_13_13(edges + 3, q) * det_and_bar[1];
-// 	return (t);
-// }
+	cprod_13_13(s, edges, q);
+	det_and_bar[3] = dot_13_13(ray, q) * det_and_bar[1];
+	if (det_and_bar[3] < 0.0f || det_and_bar[2] + det_and_bar[3] > 1.0f)
+		return (0.0f);
+	t = dot_13_13(edges + 3, q) * det_and_bar[1];
+	return (t);
+}
 
 //  Möller-Trumbore Ray-Triangle Intersection Test
-float test_triangle(t_object *obj, float ray[3], float origin[3]) 
-{
-    float edge1[3], edge2[3], h[3], s[3], q[3];
-    float det, inv_det, u, v, t;
+// float test_triangle(t_object *obj, float ray[3], float origin[3]) 
+// {
+//     float edge1[3], edge2[3], h[3], s[3], q[3];
+//     float det, inv_det, u, v, t;
 
-    // Compute edges of the triangle
-    vec_substr(obj->geo.tri.v1, obj->geo.tri.v0, edge1);
-   	vec_substr(obj->geo.tri.v2, obj->geo.tri.v0, edge2);
+//     // Compute edges of the triangle
+//     vec_substr(obj->geo.tri.v1, obj->geo.tri.v0, edge1);
+//    	vec_substr(obj->geo.tri.v2, obj->geo.tri.v0, edge2);
 
-    // Compute the determinant
-    cprod_13_13(ray, edge2, h);
-    det = dot_13_13(edge1, h);
+//     // Compute the determinant
+//     cprod_13_13(ray, edge2, h);
+//     det = dot_13_13(edge1, h);
 
-    // If the determinant is near zero, the ray is parallel to the triangle
-    if (fabs(det) < EPSILON)
-        return 0.0f;
+//     // If the determinant is near zero, the ray is parallel to the triangle
+//     if (fabs(det) < EPSILON)
+//         return 0.0f;
 
-    inv_det = 1.0f / det;
-    vec_substr(origin, obj->geo.tri.v0, s);
-
-
+//     inv_det = 1.0f / det;
+//     vec_substr(origin, obj->geo.tri.v0, s);
 
 
 
-    u = dot_13_13(s, h) * inv_det;
 
-    // Check if u is outside the triangle
-    if (u < 0.0f || u > 1.0f)
-        return 0.0f;
 
-    cprod_13_13(s, edge1, q);
-    v = dot_13_13(ray, q) * inv_det;
+//     u = dot_13_13(s, h) * inv_det;
 
-    // Check if v is outside the triangle
-    if (v < 0.0f || u + v > 1.0f)
-        return 0.0f;
+//     // Check if u is outside the triangle
+//     if (u < 0.0f || u > 1.0f)
+//         return 0.0f;
 
-    // Compute t (distance from ray origin to intersection point)
-    t = dot_13_13(edge2, q) * inv_det;
+//     cprod_13_13(s, edge1, q);
+//     v = dot_13_13(ray, q) * inv_det;
 
-    return (t); // Return t if the intersection is in front of the ray
-}
+//     // Check if v is outside the triangle
+//     if (v < 0.0f || u + v > 1.0f)
+//         return 0.0f;
+
+//     // Compute t (distance from ray origin to intersection point)
+//     t = dot_13_13(edge2, q) * inv_det;
+
+//     return (t); // Return t if the intersection is in front of the ray
+// }
 /*
 LEGACY VERSION
 
