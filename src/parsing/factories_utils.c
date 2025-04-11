be5d7564179fc4d78e18ffc562e31ec4b3511395
maricalmer <maricalmer@proton.me>
2025-04-11 17:04:45 +0200

    fix: set errno to 0

--- src/parsing/factories_utils.c
+++ src/parsing/factories_utils.c
@@ -6,7 +6,7 @@
 /*   By: dlemaire <dlemaire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 18:25:58 by dlemaire          #+#    #+#             */
-/*   Updated: 2025/04/11 16:58:10 by dlemaire         ###   ########.fr       */
+/*   Updated: 2025/04/11 17:03:55 by dlemaire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,9 @@ int	get_rgb_norm(char **specs, float *color)
 	{
 		if (!ft_isdigit(**specs))
 			return (EXIT_FAILURE);
+		errno = 0;
 		color[i] = ft_strtof(*specs, specs) / 255.0f;
-		printf("\ncolor[i]: %.6f\n", color[i]);
+		//printf("\ncolor[i]: %.6f\n", color[i]);
 		if (errno == ERANGE || (color[i] < 0.0 || color[i] > 1.0))
 		{
 			printf("\n----> first failure get_rgb_norm\n");
